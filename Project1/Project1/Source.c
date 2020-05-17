23fb4d846874b4deef68e99d00a5fe2f622acc9d
sup1337 <64952429+sup1337@users.noreply.github.com>
2020-05-17 23:32:14 +0300

    3
    3

--- Project1/Project1/Source.c
+++ Project1/Project1/Source.c
@@ -1,42 +1,75 @@
 #include "header.h"
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 char** beolvasPalya(const char* fajlNev);
 void kirajzolPalya(char** palya);
 
 int hossz, szel;
+int score = 0;
+
 
 void Game() {
 
-	int score = 0;
-		//printf("score = %i '\n'", score);
+
+
+	//printf("score = %i '\n'", score);
 	time_t currentTime, startTime;
 	startTime = time(NULL);
 	int index = 1;
 	char** palya;
+	srand(time(NULL));
+	int RandomPalya = rand() % 3;
 	palya = beolvasPalya("be.txt");
+	if (RandomPalya == 0){
+		palya = beolvasPalya("be.txt");
+}
+	else if (RandomPalya == 1) {
+		palya = beolvasPalya("be1.txt");
+	}
+	else if (RandomPalya == 2) {
+		palya = beolvasPalya("be2.txt");
+	}
 	int elozoX, elozoY;
 	int jatekosX = 1, jatekosY = 1;
 	palya[jatekosX][jatekosY] = 'P';
 	//kirajzolPalya(palya);
+	srand(time(NULL));
+	int v = rand() % 2;
 	while (1) {
-		printf("score = %i \n", score);
+		printf("score = %i** \n", score );
+
 		currentTime = time(NULL);
 		kirajzolPalya(palya);
 		printf("jobb: j\nle: l\n");
 		char option = getch();
 		elozoX = jatekosX;
 		elozoY = jatekosY;
+
 		if (option == 'd') {
 			jatekosY++;
+			if (palya[jatekosX][jatekosY] == '3' || palya[jatekosX][jatekosY] == '1') {
+				jatekosY--;
+			}
+
 		}
 		else if (option == 's') {
 			jatekosX++;
+			if (palya[jatekosX][jatekosY] == '3' || palya[jatekosX][jatekosY] == '1') {
+				jatekosX--;
+			}
 		}
 		else if (option == 'a') {
 			jatekosY--;
+			if (palya[jatekosX][jatekosY] == '3' || palya[jatekosX][jatekosY] == '1') {
+				jatekosY++;
+			}
 		}
 		else if (option == 'w') {
 			jatekosX--;
+			if (palya[jatekosX][jatekosY] == '3' || palya[jatekosX][jatekosY] == '1') {
+				jatekosX++;
+			}
 		}
 		
 		if (palya[jatekosX][jatekosY] == '0') {
@@ -44,10 +77,14 @@ void Game() {
 			palya[elozoX][elozoY] = '0';
 			
 		}
-		if (palya[jatekosX][jatekosY] == '3' || palya[jatekosX][jatekosY] == '1') {
-			jatekosX--;
-			jatekosY--;
+
+		if (palya[jatekosX][jatekosY] == 'R') {
+			palya[jatekosX][jatekosY] = 'P';
+			palya[elozoX][elozoY] = '0';
+			score++;
+				Game();
 		}
+
 
 		//Sleep(300);
 		system("CLS");
@@ -81,6 +118,8 @@ void Game() {
 
 	void kirajzolPalya(char** palya)
 	{
+		//srand(time(NULL));
+		int v = rand() % 2;
 		for (int i = 0; i < hossz; ++i) {
 			for (int j = 0; j < szel; ++j) {
 				if (palya[i][j] == '3' && (i == 0 || i == hossz - 1)) {
@@ -101,9 +140,26 @@ void Game() {
 				if (palya[i][j] == 'P') {
 					printf("@");
 				}
+				if (palya[i][j] == '4' && v == 1) {
+					printf("#");
+				}
+				else if (palya[i][j] == '4' && v == 0) {
+					printf(" ");
+				}
+				if (palya[i][j] == '5' && v == 0) {
+					printf("#");
+				}
+				else if (palya[i][j] == '5' && v == 1) {
+					printf(" ");
+				}
+
 			}
 			printf("\n");
+
 		}
+
+
+		printf("\n");
 		system("pause");
 	}
 
